fd4c3c89f4554cafbf1485703262ae9837a707e5
neogen22 <nickomjotov@yandex.ru>
2019-04-20 15:31:27 +0300

    1.3. JavaDoc. Code convention[#135816]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,14 @@
+/**
+*Hello World
+*/
+package ru.job4j.calculate;
+
+public class Calculate {
+
+	public static void main (String[] args) {
+		/** непосредственно пишем
+		Hello World
+		*/
+		System.out.println ("Hello World");
+	}
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Nikita Ometov (n@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
