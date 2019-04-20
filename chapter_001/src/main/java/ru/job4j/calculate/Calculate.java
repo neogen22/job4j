8317e66835cedb0edccd36148eebc16f069405fa
neogen22 <nickomjotov@yandex.ru>
2019-04-20 20:03:00 +0300

    1.3. JavaDoc. Code convention[#135816]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,14 +1,15 @@
-/**
-*Hello World
-*/
 package ru.job4j.calculate;
-
+/**
+*Class Calculate решение задачи 1.3 JavaDoc. Code convention
+@author Nikita
+@since 20.04.2019
+*/
 public class Calculate {
-
-	public static void main (String[] args) {
-		/** непосредственно пишем
-		Hello World
+       /** 
+		* Method main.
+		*@return Hello World
 		*/
+	public static void main (String[] args) {
 		System.out.println ("Hello World");
 	}
 }
