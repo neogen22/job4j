ab7836972d0ed950f708e349d80323db10e23bb3
neogen22 <nickomjotov@yandex.ru>
2019-04-21 21:08:36 +0300

    2.5. Инспектор кода IDEA.[#135769]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -7,7 +7,7 @@ package ru.job4j.calculate;
 public class Calculate {
        /** 
 		* Method main.
-		*@return Hello World
+		*return Hello World
 		*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
