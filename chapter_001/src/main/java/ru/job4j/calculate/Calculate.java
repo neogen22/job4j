f4265b5ef481dbaed64b84bfd30272021c43c657
neogen22 <nickomjotov@yandex.ru>
2019-04-21 17:39:51 +0300

    1.5. Checkstyle[#135818]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,8 +9,8 @@ public class Calculate {
 		* Method main.
 		*@return Hello World
 		*/
-	public static void main (String[] args) {
-		System.out.println ("Hello World");
+	public static void main(String[] args) {
+		System.out.println("Hello World");
 		
 	}
 	/**
