091b4732454e0d141e60e61fb9e0d8b876f2f7f6
neogen22 <nickomjotov@yandex.ru>
2019-04-23 20:44:51 +0300

    3.2. Конвертер валюты.[#135802]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,15 +1,19 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+    //Сложение
     public double add(double first, double second) {
         return first + second;
     }
+    //Вычитание
 	public double subtract(double first, double second) {
         return first - second;
     }
+    //Деление
 	public double div(double first, double second) {
         return first / second;
     }
+    //Умножение
 	public double multiple(double first, double second) {
         return first * second;
     }
