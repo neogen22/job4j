175b5de2f322424ff69a2b6411a03e9af9a88293
neogen22 <nickomjotov@yandex.ru>
2019-05-26 18:00:38 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.[#135795]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,12 +4,26 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = false;
         for (int out = 0; out < data.length - 1; out++) {
-                if (data [out] [out] == data [out + 1] [out + 1])  {
-                    result = true;
-                } else  {
+            if (data.length > 2) {
+                if (data[out][out] != data[data.length - 1 - out][1 + out]) {
                     result = false;
+                } else {
+                    result = true;
                 }
-        }
-        return result;
+            } else if (data.length == 2) {
+                for (out = 0; out < data.length; out++) {
+                    for (int inner = out + 1; inner < data.length; inner++) {
+                        if (data[out][inner] != data[inner][out]) {
+                            result = false;
+                        } else {
+                            result = true;
+                        }
+                    }
+                }
+            }
+        } return result;
     }
 }
+
+
+
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -11,7 +11,7 @@ public class MatrixCheckTest {
         boolean[][] input = new boolean[][] {
                 {true, true, true},
                 {false, true, true},
-                {true, false, true}
+                {true, false, true},
         };
         boolean result = check.mono(input);
         assertThat(result, is(true));
@@ -23,23 +23,23 @@ public class MatrixCheckTest {
         boolean[][] input = new boolean[][] {
                 {true, true, false},
                 {false, false, true},
-                {true, false, true}
+                {true, false, true},
         };
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
 
     @Test
-    public void whenchetDataMonoByTrueThenTrue() {
+    public void whenchetDataMonoByFalseThenFalse() {
         MatrixCheck check = new MatrixCheck();
         boolean[][] input = new boolean[][] {
-                {true, true, true, false},
+                {true, true, true, true},
                 {false, true, true, false},
                 {true, false, true, false},
                 {true, false, true, true},
         };
         boolean result = check.mono(input);
-        assertThat(result, is(true));
+        assertThat(result, is(false));
     }
 
     @Test
@@ -54,5 +54,86 @@ public class MatrixCheckTest {
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
+
+    @Test
+    public void whechetDataMonoByTrueThenFalseFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true, true},
+                {false, true, true, false},
+                {true, true, true, false},
+                {true, false, true, false},
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void lowlevelwhechetDataMonoByTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {false, true},
+                {true, false},
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void lowlevelwhechetDataMonoByTrueThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, false},
+                {true, true},
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void lowlevelwhechetDataMonoByTrueThenFalse2() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true},
+                {false, true},
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void masswhechetDataMonoByTrueThenFalseFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, true, true, false, false, true, true},
+                {false, true, true, false, false, true, true, false},
+                {true, true, true, false, false, true, true, false},
+                {true, true, true, true, true, false, true, false},
+                {true, true, true, false, true, false, true, false},
+                {true, true, true, false, false, true, true, false},
+                {true, true, true, false, false, false, true, false},
+                {true, true, true, false, false, false, true, true},
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void masswhechetDataMonoByTrueThenFalseFalse2() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {false, true, true, true, false, false, true, false},
+                {false, false, true, false, false, true, false, false},
+                {true, true, false, false, false, false, true, false},
+                {true, true, true, false, false, false, true, false},
+                {true, true, true, false, false, false, true, false},
+                {true, true, false, false, false, false, true, false},
+                {true, false, true, false, false, false, false, false},
+                {false, true, true, false, false, false, true, false},
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
 
 }
