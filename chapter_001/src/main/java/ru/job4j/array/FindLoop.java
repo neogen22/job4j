872659cdbda41408cfc22fc40be97bf4e8d42273
neogen22 <nickomjotov@yandex.ru>
2020-02-22 17:27:17 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,9 +1,9 @@
 package ru.job4j.array;
 public class FindLoop {
-    public int indexOf(int[] data, int el) {
+    public static int indexOf(int[] data, int el) {
         int rst = -1;
         /**
-          {@value} если элемента нет в массиве, то int возвращает -1.
+         {@value} если элемента нет в массиве, то int возвращает -1.
          */
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
@@ -12,5 +12,18 @@ public class FindLoop {
             }
         }
         return rst;
+
+    }
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = start; index < data.length; index++) {
+            if (index == finish) {
+                break;
+            }
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }return rst;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -9,27 +9,62 @@ public class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 5;
         int result = find.indexOf(input, value);
         int expect = 0;
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenArrayHas11Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {3, 6, 11};
+        int[] input = new int[]{3, 6, 11};
         int value = 11;
         int result = find.indexOf(input, value);
         int expect = 2;
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenArrayHas8Thenminus1() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 8;
         int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind12() {
+        int[] input = new int[]{5, 2, 10, 12, 4, 17, 22, 44};
+        int value = 12;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whennotFind17() {
+        int[] input = new int[]{5, 2, 10, 12, 4, 17, 22, 44};
+        int value = 17;
+        int start = 0;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = -1;
         assertThat(result, is(expect));
     }
