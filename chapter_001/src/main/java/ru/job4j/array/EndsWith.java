c73b5677b1328839effb36f822d427f31c2c142a
neogen22 <nickomjotov@yandex.ru>
2020-02-24 12:07:35 +0300

    6.4.1. Слово заканчивается с ... [#156317] переделка

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -3,17 +3,12 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        int wordStartLength = word.length - post.length;
-        char[]wordEndFull = new char[word.length - wordStartLength];
-        for (int i = 0; i < word.length - wordStartLength; i++) {
-            wordEndFull[i] = word[wordStartLength + i];
-        }
-        String wordEnd = String.valueOf(wordEndFull);
-        String postString = String.valueOf(post);
-        if (wordEnd.equals(postString)) {
-            return true;
-        }
-        else {
+        if (post[post.length - 1] == word[word.length - 1]) {
+            for (int i = post.length - 1; i == 0; i--) {
+                result = post[i] == word[word.length - 1];
+            }
+            return result;
+        } else {
             return false;
         }
     }
